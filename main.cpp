6bfc5e9909640c2256464f9c8657dfcd66b4dcce
qqqq4u <danila16102003@rambler.ru>
2021-12-10 00:08:18 +0300

    report added + some refactor

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <ncurses.h>
-bool isWordSuit(int *last_wotd, const int &last_word_length, int *current_word, const int &current_word_length, const int &max_len)
+bool isWordSuit(int *last_word, const int last_word_length, int *current_word, const int current_word_length, const int max_len)
 {
     if (current_word_length != max_len)
     {
@@ -12,7 +12,7 @@ bool isWordSuit(int *last_wotd, const int &last_word_length, int *current_word, 
     }
     for (size_t i = 0; i < last_word_length; i++)
     {
-        if (last_wotd[i] != current_word[i])
+        if (last_word[i] != current_word[i])
         {
             return true;
         }
@@ -27,38 +27,32 @@ int main()
     int max_lenght = 0;
     int current_length = 0;
     int *last_word = new int[100];
-    int *max_lenght_words_positions = new int[10];
-    int max_lenght_words_count = 0;
     int *current_word = new int[100];
     int n = -1;
     int *arr = new int[1000];
-    int kn = 0;
-    int qn = 0;
+    int kn = 0, qn = 0;
+    int j = 0;
     while ((c = getch()) != '.')
     {
-        // printw("%d", c);
         if ((c > 96 && c < 123) || (c == 127) || (c == 32))
         {
             if (c > 96 && c < 123 || (c == 32))
             {
                 echochar(c);
                 n++;
-                qn=n%80;
-                kn = n/80;
+                qn = n % 80;
+                kn = n / 80;
                 arr[n] = c;
             }
-            if (c == 127)
+            if (c == 127 && n != -1)
             {
-                if (n != -1)
-                {
                     arr[n] = -1;
                     move(kn, qn);
                     echochar(' ');
                     move(kn, qn);
                     n--;
-                    qn=n%80;
-                    kn = n/80;
-                }
+                    qn = n % 80;
+                    kn = n / 80;
             }
         }
     }
@@ -72,36 +66,28 @@ int main()
         nCurrent--;
     }
     n = nCurrent;
-    // for (size_t i = 0; i < n; i++)
-    // {
-    //     printw("%d ",arr[i]);
-    // }
+
     arr[++n] = '.';
 
-    int j = 0;
-    //нахождение максимальной длинны слова.
-    //запись последнего слова
     for (size_t i = 0; i < n; i++)
     {
         if (arr[i] == 32)
         {
-            if (current_length > max_lenght)
+            if (j > max_lenght)
             {
-                max_lenght = current_length;
+                max_lenght = j;
             }
             j = 0;
-            current_length = 0;
         }
         else
         {
             last_word[j] = arr[i];
             j++;
-            current_length++;
         }
     }
-    if (current_length > max_lenght)
+    if (j > max_lenght)
     {
-        max_lenght = current_length;
+        max_lenght = j;
     }
 
     j = 0;
@@ -118,13 +104,11 @@ int main()
                 echochar(' ');
             }
             j = 0;
-            current_length = 0;
         }
         else
         {
             current_word[j] = arr[i];
             j++;
-            current_length++;
         }
     }
     if (isWordSuit(last_word, max_lenght, current_word, j, max_lenght))
@@ -135,13 +119,6 @@ int main()
         }
         echochar(' ');
     }
-
-    // for (size_t i = 0; i < j; i++)
-    // {
-    //     echochar(last_word[i]);
-    // }
-
-    // printw("%d", max_lenght);
 
     while (getch() != 'q')
     {
